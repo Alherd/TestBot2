e556c34e661c068f674cbfa8b357a0afffa69996
Alherd Byk <bykalgerd@gmail.com>
2019-12-18 16:55:59 +0300

    replaced bot for new realisation

--- bot.rb
+++ bot.rb
@@ -1,13 +1,31 @@
+# frozen_string_literal: true
+
 module RubyBuild
     class Bot < SlackRubyBot::Bot
         help do
             title 'Ruby Build bot'
-            desc 'This bot for release and deploy projects'
+            desc 'This bot for release projects'
 
-            command :get_latest_release do
-                title 'get latest release'
-                desc 'description non classic'
-                long_desc 'long_description 20'
+            command 'hello' do
+                title 'hello'
+            end
+            command 'last release' do
+                title 'last release'
+            end
+            command 'release' do
+                title 'release (default: minor)'
+            end
+            command 'release major' do
+                title 'release major'
+            end
+            command 'release minor' do
+                title 'release minor'
+            end
+            command 'release bugfix' do
+                title 'release bugfix'
+            end
+            command 'release patch' do
+                title 'release patch'
             end
         end
     end
